e8a8e58f64d9b2aff1e00e9043703cba7e9ff96a
davidkarolyi <davidkarolyi96@gmail.com>
2022-10-09 15:24:50 +0200

    feat: add isAuthorized config

--- index.ts
+++ index.ts
@@ -1,14 +1,42 @@
 import { Server as WebsocketServer } from "ws";
-import { Server as HttpServer } from "http";
+import { IncomingMessage, Server as HttpServer } from "http";
+import stream from "stream";
 
 export type SignalingServerConfig = {
-  server: HttpServer;
+  httpServer: HttpServer;
+  isAuthorized: (request: IncomingMessage) => Promise<boolean> | boolean;
 };
 
 export class SignalingServer {
   private readonly websocketServer: WebsocketServer;
 
-  constructor(config: SignalingServerConfig) {
+  constructor(private readonly config: SignalingServerConfig) {
     this.websocketServer = new WebsocketServer({ noServer: true });
+
+    this.config.httpServer.on("upgrade", this.onProtocolUpgrade.bind(this));
+    this.websocketServer.on("connection", this.onSocketConnection.bind(this));
   }
+
+  private async onProtocolUpgrade(
+    request: IncomingMessage,
+    socket: stream.Duplex,
+    upgradeHead: Buffer
+  ) {
+    if (!(await this.config.isAuthorized(request))) {
+      socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+      socket.destroy();
+      return;
+    }
+
+    this.websocketServer.handleUpgrade(
+      request,
+      socket,
+      upgradeHead,
+      (websocket) => {
+        this.websocketServer.emit("connection", websocket, request);
+      }
+    );
+  }
+
+  private onSocketConnection() {}
 }
